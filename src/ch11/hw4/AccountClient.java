28e125afd36a06382f7b9ead267da59773a1ce76
Eda TUTAŞ <78338795+EdaTutas@users.noreply.github.com>
2022-08-16 11:54:31 +0300

    Sarmalama ve Erişim Yönetimi

---
+++ src/ch11/hw4/AccountClient.java
@@ -0,0 +1,167 @@
+package ch11.hw4;
+
+public class AccountClient {
+
+	
+	
+	
+	double transferCost;     // Havale Masrafı
+	double amount;			 // Para Çek - Yatır - Havale Et için Kullanılan Miktar 
+	
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private Account account;
+	
+	
+	
+	
+	
+	
+	// Constructor 
+	
+	public AccountClient(int id, String firstName , String lastName, Account account ) {
+		this.id=id;
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.account=account;
+	}
+	
+	
+	
+	
+	// Get - Set Method 
+	
+	
+	public double getTransferCost() {
+		return transferCost;
+	}
+
+
+
+	public void setTransferCost(double transferCost) {
+		this.transferCost = transferCost;
+	}
+
+
+
+	public double getAmount() {
+		return amount;
+	}
+
+
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public Account getAccount() {
+		return account;
+	}
+
+
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+
+
+	// Para Çekme 
+	
+	
+	public double withDraw(double amount) {
+		System.out.println("\n**********   Para Cekme Islemi   *******");
+		System.out.println("Islem Oncesi Bakiyeniz : " +  account.balance);
+		System.out.println("Cekilecek Miktar : " +  amount);
+		account.setBalance(account.balance-amount);
+		
+		printInfo(account);
+		return account.balance;
+		
+	}
+
+	
+	
+	
+	//Para Yatırma 
+	
+	public double deposit(double amount) {
+		System.out.println("\n\n\n**********   Para Yatirma Islemi   *******");
+		System.out.println("Islem Oncesi Bakiyeniz : " +  account.balance);
+		account.setBalance(account.balance+amount);
+		
+		printInfo(account);
+		return account.balance;
+	}
+
+
+	
+	
+	
+	/*
+	 * Havale İşlemi 
+	 * 
+	 * transferCost Havale Ücreti 	
+	 */
+	
+	
+	
+	public void transfer(double amount , double transferCost ,Account account2) {
+		System.out.println("\n\n\n**********      Havale Islemi      *******");
+		System.out.println("Islem Oncesi Bakiyeniz : " +  account.balance);
+		System.out.println("Havale Edilecek Miktar : " +  amount);
+		System.out.println("Havale Ucreti  : " +  transferCost);
+		account.setBalance(account.balance-amount-transferCost);
+		account2.setBalance(account2.balance+amount);
+		printInfo(account);
+		
+	}
+	
+	
+	
+	public void printInfo(Account account) {
+        System.out.println("\nID : " + id + "\n" + firstName + lastName + "Hesabinizdaki Tutar : " + account.balance);
+    }
+
+	
+}
---
+++ src/ch11/hw4/AccountTest.java
@@ -0,0 +1,17 @@
+package ch11.hw4;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+ 
+		Account account1 = new Account(500);
+		Account account2 = new Account(200);
+		AccountClient accountClient1 = new AccountClient( 1 ,"Eda " , "Tutas ", account1);
+		
+		accountClient1.withDraw(100);
+		accountClient1.deposit(50);
+		accountClient1.transfer(20,1,account2);
+		
+	}
+
+}
