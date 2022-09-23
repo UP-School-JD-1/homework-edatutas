70c5e1f6bae3e91dd7c06114b0fe3e1e64bf5403
Eda TUTAŞ <78338795+EdaTutas@users.noreply.github.com>
2022-09-23 16:32:18 +0300

    Java'da Fonksiyonel Programlama

--- src/course2/ch8/hw2/BookTest.java
+++ src/course2/ch8/hw2/BookTest.java
@@ -5,12 +5,11 @@ import java.util.function.BinaryOperator;
 public class BookTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Book book1 = new Book("The Cat in the Hat", "Dr", "Seuss", 450);
 		Book book2 = new Book("Harry Potter and The Sorcerers Stone", "JK", "Rowling", 411);
 		BinaryOperator<Integer> op = BinaryOperator.maxBy((a, b) -> (a > b) ? 1 : ((a == b) ? 0 : -1));
 
 		System.out.println(op.apply(book1.getPages(), book2.getPages()));
+
 	}
 }
